1499a82c24f99bd0803b008135d19c16f422e247
Mathias Moen <mathiasbbmoen@gmail.com>
2026-09-01 14:48:43 +0200

    Restructured all the files into one repo

---
+++ IT2/ordboker/test.py
@@ -0,0 +1,28 @@
+car = {
+  "brand": "Subaru",
+  "model": "Impreza",
+  "gearbox": "Manual",
+  "year": 2006
+}
+
+x = car.items()
+
+car["color"] = "Blue"
+
+print(x)
+
+
+# Create the dictionary
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 2024
+}
+# Print the model
+model = car.item
+# Add a color key
+
+# Remove the brand key
+
+# Print the dictionary
+

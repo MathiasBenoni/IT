9722eb31efdbb5c494856368810f244b1bcc6d03
Mathias Moen <mathiasbbmoen@gmail.com>
2026-09-02 14:07:27 +0200

    Added variable for the distance

--- IT1/unamed_game/JS/move.js
+++ IT1/unamed_game/JS/move.js
@@ -1,23 +1,24 @@
 const player = document.getElementById("player");
 
+const distance_moved = 10;
 let x = 100;
 let y = 100;
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "w" || event.key === "W") {
-    y -= 10;
+    y -= distance_moved;
     console.log("Forward");
   }
   if (event.key === "a" || event.key === "A") {
-    x -= 10;
+    x -= distance_moved;
     console.log("Left");
   }
   if (event.key === "s" || event.key === "S") {
-    y += 10;
+    y += distance_moved;
     console.log("Back");
   }
   if (event.key === "d" || event.key === "D") {
-    x += 10;
+    x += distance_moved;
     console.log("Right");
   }
 

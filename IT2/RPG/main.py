2fe02ba80f4a27ce9cfbd9078cce4d47eb6c27c5
Mathias Moen <mathiasbbmoen@gmail.com>
2026-09-24 08:51:14 +0200

    Progress on the game

--- IT2/RPG/main.py
+++ IT2/RPG/main.py
@@ -1,17 +1,53 @@
-class entity():
+from more_itertools import first
+
+
+class Entity():
   def __init__(self, name: str):
     self.name = name
 
-class character(entity):
+class Character(Entity):
   def __init__(self, name: str, hp: int):
     super().__init__(name)
     self.hp = hp
 
-class player(character):
-  def __init__(self, name, hp):
+class Player(Character):
+  def __init__(self, name: str, hp: int):
     super().__init__(name, hp)
-  
 
-mathias = character("Mathias", 100)
+class Enemy(Character):
+  def __init__(self, name: str, hp: int, dmg: int, sentience: bool):
+    super().__init__(name, hp)
+    self.dmg = dmg
+    self.sentience = sentience
 
-print(mathias)
+def stats(obj):
+  if isinstance(obj, Enemy):
+    print()
+    print(f"Name: {obj.name}")
+    print(f"HP: {obj.hp}")
+    print(f"Damage: {obj.dmg}")
+    print(f"Sentience: {obj.sentience}")
+    print()
+
+name = input("What are you called? ")
+player = Player(name, 100)
+
+first_enemy = Enemy("Bob", 10, 0, True)
+
+print(player.name)
+print(player.hp)
+
+print(f"You encounter {first_enemy.name}!")
+print("=======================")
+print("Actions:")
+print()
+print("1. See stats on the poor fella")
+print("2. Kill the poor fella")
+print("3. Take avoiding action and go around him")
+print("4. Nothing")
+
+choice = input("What do you do (1 - 4)? ")
+
+if choice == "1":
+  stats(first_enemy)
+
